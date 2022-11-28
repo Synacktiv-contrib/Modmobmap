c81fb2b72af2b1f3e39a5cb8f2441e8746fc49af
Sébastien Dudek <fluxius@gmail.com>
2022-11-28 22:05:32 +0100

    Updating  gr-gsm

--- install_all-Ubuntu_22.04.sh
+++ install_all-Ubuntu_22.04.sh
@@ -19,13 +19,20 @@ libsdl1.2-dev libgsl-dev libqwt-qt5-dev libqt5opengl5-dev python3-pyqt5 \
 liblog4cpp5-dev libzmq3-dev python3-yaml python3-click python3-click-plugins \
 python3-zmq python3-scipy python3-gi python3-gi-cairo gobject-introspection gir1.2-gtk-3.0
 
-echo "[+] Installing gr-osmosdr package"
-sudo apt install gr-osmosdr
-
-echo "[+] Downloading gr-gsm for Python3 and GNU Radio 3.10"
+echo "[+] Installing gr-osmosdr from source"
 mkdir thirdparty
 cd thirdparty
 REMPATH=`pwd`
+#sudo apt install gr-osmosdr
+git clone https://github.com/osmocom/gr-osmosdr
+cd gr-osmosdr
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local ../
+make -j$(nproc)
+sudo make install
+cd $REMPATH
+echo "[+] Downloading gr-gsm for Python3 and GNU Radio 3.10"
 git clone https://github.com/bkerler/gr-gsm
 echo "[+] Building and installing gr-gsm for GNU Radio 3.10"
 cd gr-gsm
