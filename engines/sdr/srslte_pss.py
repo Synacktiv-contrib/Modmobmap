92b189d318ac6c6e3f1ba6806e23786e97041901
Sébastien Dudek <fluxius@gmail.com>
2023-02-19 10:01:02 +0100

    Adding contributor name to srs module

--- engines/sdr/srslte_pss.py
+++ engines/sdr/srslte_pss.py
@@ -6,6 +6,9 @@
 # <sebastien.dudek(<@T>)penthertz.com> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return FlUxIuS ;)
+# 
+# We'd like to thank our contributors for maintening this code:
+# - @h0rac (Grzegorz Wypych) from PWNSec.pl
 # ----------------------------------------------------------------------------
 
 from __future__ import print_function
